1442b5f8d12407a0908761ccc499a4babf6956ad
henophilia <tech@henophilia.org>
2024-07-14 22:14:02 +0200

    feat: adjust to Henophilia

--- content/config.mjs
+++ content/config.mjs
@@ -2,4 +2,18 @@
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {};
+export default {
+  title: "Henophilia",
+  author: "Henophilia",
+  authorUrl: "https://henophilia.org",
+  analytics: "G-FKY5DGPBKZ",
+  description: "Building bridges of pleasure between reality and flourishing.",
+  domain: "https://henophilia.org",
+  navbarTitle: {
+    text: "Henophilia",
+  },
+  socialLinks: {
+    instagram: "https://www.instagram.com/henophilia/",
+  },
+  theme: { default: "" },
+};
